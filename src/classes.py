cc16e184643aaeb4bc35db4f038e3f0e913810c0
Conrad <20105131+shaives@users.noreply.github.com>
2024-05-21 09:22:29 -0700

    cleaned imports

--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,5 @@
 import sys
+
 from math import *
 
 import cplex
--- src/functions.py
+++ src/functions.py
@@ -3,8 +3,6 @@ import re
 
 from math import *
 
-
-
 # Euclidean distance between two points
 def d(x0, y0, x1, y1):
 	return sqrt((x0-x1)**2 + (y0-y1)**2)
