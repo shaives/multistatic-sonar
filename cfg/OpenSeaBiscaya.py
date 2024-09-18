13430c31c572ea0966548b1c2aaccb4436079706
Conrad <20105131+shaives@users.noreply.github.com>
2024-09-18 10:32:22 -0700

    cleaned up file

--- cfg/OpenSeaBiscaya.py
+++ cfg/OpenSeaBiscaya.py
@@ -6,15 +6,15 @@ RAM =2*8192	# 0=use disk, 1=use RAM for storing data
 X = 10 # number of pixels in x-direction
 Y = 10 # number of pixels in y-direction
 
-GOAL = 1 # optimization goal: cover all pixels, minimize cost for deployed equipment (0) or deploy equipment, maximize coverage (1)
+GOAL = 0 # optimization goal: cover all pixels, minimize cost for deployed equipment (0) or deploy equipment, maximize coverage (1)
 
-S = 1 # EITHER: cost for each deployed source (if GOAL=0), OR: number of deployed sources (if GOAL=1)
-R = 4 # EITHER: cost for each deployed receiver (if GOAL=0), OR: number of deployed receivers (if GOAL=1)
+S = 10 # EITHER: cost for each deployed source (if GOAL=0), OR: number of deployed sources (if GOAL=1)
+R = 1 # EITHER: cost for each deployed receiver (if GOAL=0), OR: number of deployed receivers (if GOAL=1)
 
-rho_0 = 8 # range of the day (in pixels)
+rho_0 = 2 # range of the day (in pixels)
 rb = 1 # pulse length (for direct-blast-effect) (in pixels)
 
-CC = 1 # cookie cutter model (1), or probabilistic model (0)
+CC = 0 # cookie cutter model (1), or probabilistic model (0)
 
 dp = 0.95 # desired minimum detection probability per area; only relevant for probabilistic model
 pmax = 0.95 # maximum detection probability for single sender/receiver pair (remark: never set probability to 1, because of taking the log); only relevant for probabilistic model
@@ -33,23 +33,6 @@ USERCUTS = 0 # 0=no user cuts, 1=user cuts on
 
 USERCUTSTRENGTH = 1.0 # how deep must user cuts be to be separated?
 
-MODELTYPE = 10 # select model type: 
-  # 0=MINLP/let cplex do the job 
-  # 1=Fortet (1959), Dantzig (1960), Balas (1964), Zangwill (1965), Watters (1967) 
-  # 2=Glover, Woolsey (1974) 
-  # 3=Glover (1975) for receivers
-  # 4=Glover (1975) for sources
-  # 5=Glover (1975) for sources&receivers 
-  # 6=1st Oral-Kettani (1992) for receivers
-  # 7=1st Oral-Kettani (1992) for sources 
-  # 8=1st Oral-Kettani (1992) for sources&receivers 
-  # 9=2nd Oral-Kettani (1992) for receivers
-  # 10=2nd Oral-Kettani (1992) for sources
-  # 11=2nd Oral-Kettani (1992) for sources&receivers
-  # 12=Chaovalitwongse, Pardalos, Prokopyev (2004) for receivers
-  # 13=Chaovalitwongse, Pardalos, Prokopyev (2004) for sources
-  # 14=Chaovalitwongse, Pardalos, Prokopyev (2004) for sources&receivers
-  # 15=Rodrigues et al. (2014) 
 
 HEURISTIC = 10 # 0=no heuristic, >0: with heuristic, number of rounds
 
