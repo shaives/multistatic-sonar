ab7f9ec189114d01e6bd7996fe52863984de1fe1
Conrad <20105131+shaives@users.noreply.github.com>
2024-10-06 15:44:18 -0700

    cleaned file

--- cfg/Agadir.py
+++ cfg/Agadir.py
@@ -3,18 +3,16 @@ INPUT 	= "GMRTv3_4_20171109topo.asc"   # file name of ocean floor data
 
 RAM = 8192	# 0=use disk, 1=use RAM for storing data
 
-X = 12      # number of pixels in x-direction
-Y = 12      # number of pixels in y-direction
+X = 10      # number of pixels in x-direction
+Y = 10      # number of pixels in y-direction
 
-GOAL = 1    # optimization goal: cover all pixels, minimize cost for deployed equipment (0), or: deploy equipment, maximize coverage (1)
+GOAL = 0    # optimization goal: cover all pixels, minimize cost for deployed equipment (0), or: deploy equipment, maximize coverage (1)
 
-S = 3     # EITHER: cost for each deployed source (if GOAL=0), OR: number of deployed sources (if GOAL=1)
-R = 12       # EITHER: cost for each deployed receiver (if GOAL=0), OR: number of deployed receivers (if GOAL=1)
+S = 10     # EITHER: cost for each deployed source (if GOAL=0), OR: number of deployed sources (if GOAL=1)
+R = 1      # EITHER: cost for each deployed receiver (if GOAL=0), OR: number of deployed receivers (if GOAL=1)
 
-rho_0 = 3   # range of the day (in pixels)
-rb = 0.3    # pulse length (for direct-blast-effect) (in pixels)
-
-CC = 1      # cookie cutter model (1), or probabilistic model (0)
+rho_0 = 2   # range of the day (in pixels)
+rb = 1    # pulse length (for direct-blast-effect) (in pixels)
 
 dp = 0.95   # desired minimum detection probability per area; only relevant for probabilistic model
 pmax = 0.95 # maximum detection probability for single sender/receiver pair (remark: never set probability to 1, because of taking the log); only relevant for probabilistic model
@@ -26,20 +24,14 @@ TS = [(0.0,0.1),(30.0,0.4),(75.0,0.3),(90.0,1.0)] # target strength (in pixels),
 #TS = [] 	# if TS is an empty list, the target angle is not considered
 STEPS = 30 	# step size for discretization of half-circle (eg., "10" degrees gives 0,10,20,...,170)
 
-BOUND = 0 	# for models 3-14: 0=individual bound per row, 1=min/max over all rows
+BOUND = 1 	# for models 3-14: 0=individual bound per row, 1=min/max over all rows
 
 USERCUTS = 0 # 0=no user cuts, 1=user cuts on
 
 USERCUTSTRENGTH = 1.0 # how deep must user cuts be to be separated?
 
-MODELTYPE = 10 # select model type: 
-	# 7=1st Oral-Kettani (1992) for sources 
-	# 10=2nd Oral-Kettani (1992) for sources
-	# 13=Chaovalitwongse, Pardalos, Prokopyev (2004) for sources
-
-
 HEURISTIC = 100 # 0=no heuristic, >0: with heuristic, number of rounds
 
 SOLVE = 2 # 0=only root relaxation, 1=root+cuts, 2=to the end (optimality or timelimit reached)
 
-TIMELIMIT = 36000 # time limit in seconds
+TIMELIMIT = 3600 # time limit in seconds
