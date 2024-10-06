4a029e809ffa6f0b47e0201bd05c9f242b7bbef2
Conrad <20105131+shaives@users.noreply.github.com>
2024-10-06 15:44:36 -0700

    Cleaned file

--- cfg/OpenSeaBiscaya.py
+++ cfg/OpenSeaBiscaya.py
@@ -14,17 +14,14 @@ R = 1 # EITHER: cost for each deployed receiver (if GOAL=0), OR: number of deplo
 rho_0 = 2 # range of the day (in pixels)
 rb = 1 # pulse length (for direct-blast-effect) (in pixels)
 
-CC = 0 # cookie cutter model (1), or probabilistic model (0)
-
 dp = 0.95 # desired minimum detection probability per area; only relevant for probabilistic model
 pmax = 0.95 # maximum detection probability for single sender/receiver pair (remark: never set probability to 1, because of taking the log); only relevant for probabilistic model
 pmin = 0.1 # minimum detection probability for single sender/receiver pair; only relevant for probabilistic model
 b1 = 0.2 # parameter to describe how fast the detection probability decays at the outer (range) boundary; only relevant for probabilistic model
 b2 = 0.1 # parameter to describe how fast the detection probability decays at the inner (direct blast) boundary; only relevant for probabilistic model
 
-TS = [(0.0,0.1),(30.0,0.4),(75.0,0.3),(90.0,1.0)] # target strength (in pixels), added to the range of the day, 0 (degree) = bow/stern, 90 (degree) = beam 
-#TS = [] # if TS is an empty list, the target angle is not considered
-
+#TS = [(0.0,0.1),(30.0,0.4),(75.0,0.3),(90.0,1.0)] # target strength (in pixels), added to the range of the day, 0 (degree) = bow/stern, 90 (degree) = beam 
+TS = [] # if TS is an empty list, the target angle is not considered
 STEPS = 30 # step size for discretization of half-circle (eg., "10" degrees gives 0,10,20,...,170)
 
 BOUND = 1 # 0=individual bound per row, 1=min/max over all rows
@@ -33,9 +30,8 @@ USERCUTS = 0 # 0=no user cuts, 1=user cuts on
 
 USERCUTSTRENGTH = 1.0 # how deep must user cuts be to be separated?
 
-
-HEURISTIC = 10 # 0=no heuristic, >0: with heuristic, number of rounds
+HEURISTIC = 100 # 0=no heuristic, >0: with heuristic, number of rounds
 
 SOLVE = 2 # 0=only root relaxation, 1=root+cuts, 2=to the end (optimality or timelimit reached)
 
-TIMELIMIT = 360 # time limit in seconds
+TIMELIMIT = 3600 # time limit in seconds
