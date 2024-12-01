e275dac5019209bcd786f1c6c362e7042c21abe0
shaives <20105131+shaives@users.noreply.github.com>
2024-12-01 10:58:29 -0800

    renamed shell scripts and added working multijob

--- bisonnode.sh
+++ bisonnode.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 #SBATCH --job-name node-multistatic-sonar
-#SBATCH --nodes=1 
+#SBATCH --nodes=1                               # How many nodes do you want to use? You probably want 1.
 #SBATCH --ntasks=1                              # How many tasks (i.e. processors w/ distributed memory) do you want? You probably want 1 here unless using MPI.
-#SBATCH --cpus-per-task=16                      # How many cores (i.e. threads w/ shared memory) per processor do you want?
+#SBATCH --cpus-per-task=32                      # How many cores (i.e. threads w/ shared memory) per processor do you want?
 #SBATCH --mem=64GB                              # How much memory do you want total? (You can use suffixes: M=MB, G=GB, T=TB.)
 #SBATCH --nodelist=compute-3-[1,5,7,9,11]       # Which nodes do you want to use?
 #SBATCH --exclusive                             # Don't share nodes with other users
 #SBATCH --gres=gpu:0                            # Make sure that you do not request a GPU if you do not use an appropriate partition.
 #SBATCH --output=logs/%j_out.txt                # path for logs
-#SBATCH --error=logs/%j_error.txt
+#SBATCH --error=logs/%j_error.txt               # path for errors
 #SBATCH --time 1-00:00:00                       # max time running on HPC days-hours:minutes:secounds
 #SBATCH --mail-user conrad.urban.gy@nps.edu
-#SBATCH --mail-type ALL
+#SBATCH --mail-type ALL                         # Send email on job start, end, and fail
 
 # Print job parameters to log file
 echo "Job Parameters:"
--- bisonranch.sh
+++ bisonranch.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #SBATCH --job-name multistatic-sonar-gu
+#SBATCH --array=0-4                             # How many different jobs do you want to run? You probably want 1.
 #SBATCH --nodes=1                               # How many nodes do you want to use? You probably want 1.
 #SBATCH --ntasks=1                              # How many tasks (i.e. processors w/ distributed memory) do you want? You probably want 1 here unless using MPI.
 #SBATCH --cpus-per-task=32                      # How many cores (i.e. threads w/ shared memory) per processor do you want?
-#SBATCH --mem=64GB                              # How much memory do you want total? (You can use suffixes: M=MB, G=GB, T=TB.)
+#SBATCH --mem=32GB                              # How much memory do you want total? (You can use suffixes: M=MB, G=GB, T=TB.)
 #SBATCH --gres=gpu:0                            # Make sure that you do not request a GPU if you do not use an appropriate partition.
-#SBATCH --output=logs/%j_out.txt                # path for logs
-#SBATCH --error=logs/%j_error.txt               # path for errors
-#SBATCH --time 1-00:00:00                       # max time running on HPC days-hours:minutes:secounds
+#SBATCH --output=logs/%j_%a_out.txt             # path for logs
+#SBATCH --error=logs/%j_%a_error.txt            # path for errors
+#SBATCH --time 1-12:00:00                       # max time running on HPC days-hours:minutes:secounds
 #SBATCH --mail-user conrad.urban.gy@nps.edu
 #SBATCH --mail-type ALL                         # Send email on job start, end, and fail
 
@@ -29,4 +30,6 @@ Start time: $(date)
 . /etc/profile
 source /smallwork/$USER/myenv1/bin/activate
 
-python bison.py MontereyPeninsular
+ARGS=(Agadir EnglishChannel MontereyPeninsular OpenSeaBiscaya0 OpenSeaBiscaya1)
+
+python bison.py ${ARGS[$SLURM_ARRAY_TASK_ID]}
