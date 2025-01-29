6bf7452dee6c05c6663686ecdd34fe8000e682e6
Conrad <20105131+shaives@users.noreply.github.com>
2025-01-28 16:55:34 -0800

    removed main function

--- bison.py
+++ bison.py
@@ -6,7 +6,6 @@ import time
 import os
 import shutil
 import importlib
-import json
 
 from math import *
 from datetime import datetime
@@ -20,127 +19,128 @@ from src.optimization import *
 # --- let's start
 # ---------------------------------------------------
 
-def main():
 
-    try:
+try:
 
-        filename = sys.argv[1]
+    filename = sys.argv[1]
 
-        filepath = os.path.join("cfg", filename + '.py')
+    filepath = os.path.join("cfg", filename + '.py')
 
-        if os.path.isfile(filepath):
+    if os.path.isfile(filepath):
 
-            instance = importlib.import_module("cfg." + filename)
-            start_time = time.time()
+        instance = importlib.import_module("cfg." + filename)
+        start_time = time.time()
 
-    except IndexError:
+except IndexError:
 
-        print("File not found in the 'cfg' folder.")
-    
-    # creating an output directory and copy config file
-    if not os.path.exists('outputs'):
-        os.mkdir('outputs')
-    outdir = 'outputs/' + datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '_' + filename
-    os.mkdir(outdir)
-    shutil.copy2("./cfg/" + filename + ".py", outdir)
+    print("File not found in the 'cfg' folder.")
 
-    start_time = time.time()
+# creating an output directory and copy config file
+if not os.path.exists('outputs'):
+    os.mkdir('outputs')
 
-    # redirect output to screen and logfile
-    sys.stdout = Logger(outdir) 
+outdir = 'outputs/' + datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '_' + filename
+os.mkdir(outdir)
+shutil.copy2("./cfg/" + filename + ".py", outdir)
 
-    print(f"BISON - BIstatic Sonar OptimizatioN")
-    print(f"")
-    print(f"                                                                                           ")
-    print(f"                                   @@@@@@@@@                                               ")
-    print(f"                               @@@@         @@@@                                           ")
-    print(f"                            @@@                 @@@                                        ")
-    print(f"                         @@@                       @@@@                                    ")
-    print(f"                       @@                             @@@@@                                ")
-    print(f"                     @@@                                   @@@@                            ")
-    print(f"                   @@@                                         @@@@                        ")
-    print(f"         @@@@@@@@@@                                                @@@@                    ")
-    print(f"     @@@@                                   @                          @@@@@               ")
-    print(f"   @@                                       @@                              @@@@@@         ")
-    print(f"  @@           @                              @@                              @@@@@        ")
-    print(f" @@            @@                              @                                 @@@@      ")
-    print(f" @            @ @                               @                                  @@@     ")
-    print(f" @            @ @                               @                                    @@    ")
-    print(f" @          @@@ @@                             @@                                   @  @   ")
-    print(f"@@            @@@                             @                                     @@  @  ")
-    print(f" @@                                           @@               @@                   @@@ @@ ")
-    print(f"  @                                         @@                   @                  @ @@@@ ")
-    print(f"   @@         @                             @                     @@                @ @ @@ ")
-    print(f"     @@    @@@                               @                     @@              @@ @ @  ")
-    print(f"      @@                                     @@                 @@@  @             @ @@@@  ")
-    print(f"        @                                    @@               @@     @@@          @@ @  @@ ")
-    print(f"        @                                     @@     @@@@@@@@@     @@   @@@@        @@   @@")
-    print(f"         @                 @                  @@@@@@@       @     @         @@@       @   @")
-    print(f"          @@@@@@@@@        @@                @@             @     @            @      @@ @ ")
-    print(f"                   @@@   @@  @              @               @    @              @@    @@   ")
-    print(f"                      @  @    @            @               @    @                @@   @@   ")
-    print(f"                       @@      @@          @             @@    @                 @@   @@   ")
-    print(f"                       @@       @@    @@@@@            @@@   @@                  @@   @@   ")
-    print(f"                               @@@  @@@              @@    @@                    @@   @@   ")
-    print(f"                             @@    @@              @@@@@@@@                     @    @@@   ")
-    print(f"                             @@@@@@@@                                          @@@@@@@     ")
-    print(f"")
-    print(f"")
+start_time = time.time()
 
-    print(f"Called")
-    exit()
-    # ---------------------------------------------------
-    # --- read ocean elevation data file
-    # ---------------------------------------------------
+# redirect output to screen and logfile
+sys.stdout = Logger(outdir) 
 
-    map, ocean, ocean_surface, min_depth, max_depth = reading_in_ocean_data(instance)
+print(f"BISON - BIstatic Sonar OptimizatioN")
+print(f"")
+print(f"                                                                                           ")
+print(f"                                   @@@@@@@@@                                               ")
+print(f"                               @@@@         @@@@                                           ")
+print(f"                            @@@                 @@@                                        ")
+print(f"                         @@@                       @@@@                                    ")
+print(f"                       @@                             @@@@@                                ")
+print(f"                     @@@                                   @@@@                            ")
+print(f"                   @@@                                         @@@@                        ")
+print(f"         @@@@@@@@@@                                                @@@@                    ")
+print(f"     @@@@                                   @                          @@@@@               ")
+print(f"   @@                                       @@                              @@@@@@         ")
+print(f"  @@           @                              @@                              @@@@@        ")
+print(f" @@            @@                              @                                 @@@@      ")
+print(f" @            @ @                               @                                  @@@     ")
+print(f" @            @ @                               @                                    @@    ")
+print(f" @          @@@ @@                             @@                                   @  @   ")
+print(f"@@            @@@                             @                                     @@  @  ")
+print(f" @@                                           @@               @@                   @@@ @@ ")
+print(f"  @                                         @@                   @                  @ @@@@ ")
+print(f"   @@         @                             @                     @@                @ @ @@ ")
+print(f"     @@    @@@                               @                     @@              @@ @ @  ")
+print(f"      @@                                     @@                 @@@  @             @ @@@@  ")
+print(f"        @                                    @@               @@     @@@          @@ @  @@ ")
+print(f"        @                                     @@     @@@@@@@@@     @@   @@@@        @@   @@")
+print(f"         @                 @                  @@@@@@@       @     @         @@@       @   @")
+print(f"          @@@@@@@@@        @@                @@             @     @            @      @@ @ ")
+print(f"                   @@@   @@  @              @               @    @              @@    @@   ")
+print(f"                      @  @    @            @               @    @                @@   @@   ")
+print(f"                       @@      @@          @             @@    @                 @@   @@   ")
+print(f"                       @@       @@    @@@@@            @@@   @@                  @@   @@   ")
+print(f"                               @@@  @@@              @@    @@                    @@   @@   ")
+print(f"                             @@    @@              @@@@@@@@                     @    @@@   ")
+print(f"                             @@@@@@@@                                          @@@@@@@     ")
+print(f"")
+print(f"")
 
-    # ---------------------------------------------------
-    # --- create outputs
-    # ---------------------------------------------------
+print(f"Called")
 
-    # output latex map
-    create_latex_map(instance, map, min_depth, max_depth, outdir)
+# ---------------------------------------------------
+# --- read ocean elevation data file
+# ---------------------------------------------------
 
-    # output ocean pixels
-    create_ocean_dat(ocean, outdir)
+map, ocean, ocean_surface, min_depth, max_depth, depth_layer_hight = reading_in_ocean_data(instance)
 
-    # output map
-    create_map_dat(instance, map, outdir)
+# ---------------------------------------------------
+# --- create outputs
+# ---------------------------------------------------
 
-    # plot function g
-    create_plot_func_g(instance, outdir)
+# output latex map
+create_latex_map(instance, map, min_depth, max_depth, outdir)
 
-    # ---------------------------------------------------
-    # --- compute coverage
-    # ---------------------------------------------------
+# output ocean pixels
+create_ocean_dat(ocean, outdir)
 
-    print(f"Computing coverage")
+# output map
+create_map_dat(instance, map, outdir)
 
-    detection_prob = compute_coverage_triples(instance, ocean, ocean_surface)
+# plot function g
+create_plot_func_g(instance, outdir)
+print(f"{min_depth}, {max_depth}, {depth_layer_hight}")
+exit()
+# ---------------------------------------------------
+# --- compute coverage
+# ---------------------------------------------------
 
-    # ---------------------------------------------------
-    # --- computing the rowsum in detection_prob
-    # ---------------------------------------------------
+print(f"Computing coverage")
 
-    print(f"Computing detection prob")
+detection_prob = compute_coverage_triples(instance, ocean, ocean_surface)
 
-    detection_prob_rowsum_r, detection_prob_rowsum_s = compute_rowsum_detection_prob(instance, ocean, ocean_surface, detection_prob)
+# ---------------------------------------------------
+# --- computing the rowsum in detection_prob
+# ---------------------------------------------------
 
-    # ---------------------------------------------------
-    # --- set up & compute optimization model
-    # ---------------------------------------------------
+print(f"Computing detection prob")
 
-    print(f"Create optimization model")
+detection_prob_rowsum_r, detection_prob_rowsum_s = compute_rowsum_detection_prob(instance, ocean, ocean_surface, detection_prob)
 
-    model = create_optimization_model(instance, ocean_surface, ocean, detection_prob_rowsum_s, detection_prob)
-    
-    print(f"Solve optimization model")
-    
-    solve_model(model, instance, ocean_surface, outdir, 'gurobi')  # or 'cplex', 'gurobi', etc.
+# ---------------------------------------------------
+# --- set up & compute optimization model
+# ---------------------------------------------------
 
-    # ---------------------------------------------------
-    # --- output optimization model results
-    # ---------------------------------------------------
+print(f"Create optimization model")
 
-    output_solution(model, instance, ocean_surface, ocean, detection_prob, map, min_depth, max_depth, outdir, start_time)
+model = create_optimization_model(instance, ocean_surface, ocean, detection_prob_rowsum_s, detection_prob)
+
+print(f"Solve optimization model")
+
+solve_model(model, instance, ocean_surface, outdir, 'gurobi')  # or 'cplex', 'gurobi', etc.
+
+# ---------------------------------------------------
+# --- output optimization model results
+# ---------------------------------------------------
+
+output_solution(model, instance, ocean_surface, ocean, detection_prob, map, min_depth, max_depth, outdir, start_time)
