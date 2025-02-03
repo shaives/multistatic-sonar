dbfc81756a5147b70ea7363ffabe212848da396c
shaives <20105131+shaives@users.noreply.github.com>
2025-02-03 10:38:03 -0800

    renamed shell scripts for running on HPC

--- ranch.sh
+++ ranch.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
-#SBATCH --job-name 20x20_small_roh0
+#SBATCH --job-name bison_cplex
+#SBATCH --array=0-1                             # How many different jobs do you want to run? You probably want 1.
 #SBATCH --nodes=1                               # How many nodes do you want to use? You probably want 1.
 #SBATCH --ntasks=1                              # How many tasks (i.e. processors w/ distributed memory) do you want? You probably want 1 here unless using MPI.
 #SBATCH --cpus-per-task=32                      # How many cores (i.e. threads w/ shared memory) per processor do you want?
 #SBATCH --mem=128GB                              # How much memory do you want total? (You can use suffixes: M=MB, G=GB, T=TB.)
 #SBATCH --gres=gpu:0                            # Make sure that you do not request a GPU if you do not use an appropriate partition.
-#SBATCH --output=logs/%j_out.txt                # path for logs
-#SBATCH --error=logs/%j_error.txt               # path for errors
-#SBATCH --time 7-00:00:00                       # max time running on HPC days-hours:minutes:secounds
+#SBATCH --output=logs/%j_%a_out.txt             # path for logs
+#SBATCH --error=logs/%j_%a_error.txt            # path for errors
+#SBATCH --time 1-00:00:00                       # max time running on HPC days-hours:minutes:secounds
 #SBATCH --mail-user conrad.urban.gy@nps.edu
 #SBATCH --mail-type ALL                         # Send email on job start, end, and fail
+
+ARGS=(MontereyPeninsular_0_new MontereyPeninsular_1_new)
 
 # Print job parameters to log file
 echo "Job Parameters:"
 echo "===============================
 Job name: $SLURM_JOB_NAME
 Job ID: $SLURM_JOB_ID
+Area: ${ARGS[$SLURM_ARRAY_TASK_ID]}
 Submitted from: $SLURM_SUBMIT_HOST
 Running on node: $SLURM_NODELIST
 Number of nodes: $SLURM_NNODES
@@ -29,4 +33,4 @@ Start time: $(date)
 . /etc/profile
 source /smallwork/$USER/myenv1/bin/activate
 
-python bison.py MontereyPeninsular
+python bison.py ${ARGS[$SLURM_ARRAY_TASK_ID]}
