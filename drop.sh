a4c28dfef2003d405547b87a1ad421e2d2d1f068
Conrad <20105131+shaives@users.noreply.github.com>
2025-02-14 12:37:46 -0800

    standart config now

--- drop.sh
+++ drop.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#SBATCH --job-name bison_error_0
+#SBATCH --job-name bison_*                      # Job name
 #SBATCH --nodes=1                               # How many nodes do you want to use? You probably want 1.
 #SBATCH --ntasks=1                              # How many tasks (i.e. processors w/ distributed memory) do you want? You probably want 1 here unless using MPI.
-#SBATCH --cpus-per-task=32                      # How many cores (i.e. threads w/ shared memory) per processor do you want?
-#SBATCH --mem=128GB                              # How much memory do you want total? (You can use suffixes: M=MB, G=GB, T=TB.)
+#SBATCH --cpus-per-task=64                      # How many cores (i.e. threads w/ shared memory) per processor do you want?
+#SBATCH --mem=256GB                             # How much memory do you want total? (You can use suffixes: M=MB, G=GB, T=TB.)
 #SBATCH --gres=gpu:0                            # Make sure that you do not request a GPU if you do not use an appropriate partition.
 #SBATCH --output=logs/%j_out.txt                # path for logs
 #SBATCH --error=logs/%j_error.txt               # path for errors
@@ -29,4 +29,4 @@ Start time: $(date)
 . /etc/profile
 source /smallwork/$USER/myenv1/bin/activate
 
-python bison.py MontereyPeninsular_1_new
+python bison.py Iceland_cost
